3e84d416d86f1a4dbb7c0d753a269ead326f3c4b
Lance Denes <lancetdenes@i21a-s1.ufhpc>
2016-09-16 11:33:21 -0400

    change to give yesterdays date

--- weather.sh
+++ weather.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-curl "https://www.wunderground.com/history/airport/GNV/2016/09/06/DailyHistory.heml?&format=1" > gnv.txt
+a=`date -d 'yesterday' '+%Y/%m/%d'` 
+
+curl "https://www.wunderground.com/history/airport/GNV/$a/DailyHistory.heml?&format=1" > gnv.txt 
 
 maxTemp=`awk -F',' '{print $2}' gnv.txt | sort -n | tail -n1`
 
-echo The Max temp is $maxTemp
+echo Yesterday the high was $maxTemp
